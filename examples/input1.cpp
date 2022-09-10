2f9f3bc198ba68de01618992fd947595c7d1cce1
Joonas Miettinen <joonas.m.miettinen@gmail.com>
2022-09-10 11:20:02 +0300

    feat: wip extract() improvements

--- examples/input1.cpp
+++ examples/input1.cpp
@@ -1,9 +1,30 @@
 
-// @codedoc_comment_block key1
-// @codedoc_comment_block key2
-// Comment line 1
-// Comment line 2
-// Comment line 3
-// @codedoc_comment_block key2
-// @codedoc_comment_block key1
+// @block key_1
+// @block key_2
+// 4
+// 5
+// 6
+// @block key_2
+// @block key_1
+
+// @chunk key_3
+// 11
+// @chunk key_4
+// 13
+
+// @block key_5
+// @chunk key_6
+// 17
+// @block key_5
+
+// @doc_start key_7
+// @chunk key_6
+// 22
+// @doc_stop key_7
+
+// @block key_8
+// 26
+int my_var = 1;
+// 28
+// @block key_8
 
--- src/tbe/extract.hpp
+++ src/tbe/extract.hpp
@@ -11,10 +11,10 @@
 namespace extract {
 void extract(
     std::string                                             &file_path,
-    std::function<bool(std::string)>                        &doc_begins,
-    std::function<bool(std::string)>                        &doc_includes,
-    std::function<bool(std::string line, std::string key)>  &doc_ends,
-    std::function<bool(std::string)>                        &doc_ended,
+    std::function<bool(std::string)>                        &section_starts,
+    std::function<bool(std::string)>                        &section_includes,
+    std::function<bool(std::string line, std::string key)>  &section_stops,
+    // std::function<bool(std::string)>                        &section_ended,
     std::function<bool(std::string)>                        &key_allowed,
     std::function<std::string(std::string)>                 &key_extract,
     std::function<std::string(std::string)>                 &line_extract,
@@ -24,75 +24,110 @@ void extract(
     std::ifstream file_conn;
     file_conn.open(file_path);
 
-    std::vector<std::string> key_by_block(0);
+    // @doc extract
+    // `extract` maintains a list of active all extracted sections
+    // and currently active sections. Active sections are those whose key
+    // have been encountered and who have not yet been marked to have ended
+    // (see `section_stops`, `section_ended`). For instance, a section can end
+    // when the same key is encountered a second time.
+    std::vector<std::string> key_by_section(0);
 
-    std::vector<int>            active_block_no_set(0);
-    std::vector<std::string>    active_key_set(0);
+    std::vector<int>            active_section_no_set(0);
+    std::vector<std::string>    active_section_key_set(0);
     int line_no = -1;
     std::string line;
     while (std::getline(file_conn, line)) {
         line_no += 1;
-        // @codedoc_chunk extract_keyed_comment_blocks
-        // Function `extract_keyed_comment_blocks` goes through the file
-        // at `file_path` one line at a time. A line can only be
-        // in a comment block if the callback function `doc_includes`
-        // returns `true` for the line.
-        bool is_included = doc_includes(line);
-        // @codedoc_chunk extract_keyed_comment_blocks
-        // If `doc_ended` returns `true` for the line, we continue to the
-        // next line.
-        bool is_ended = doc_ended(line);
-        if (is_ended) {
-            continue; // done here, go to next line
-        }
+        // @doc extract
+        // `extract` goes through the file at `file_path` one line at a time.
+
+        // @-doc extract::section_ended
+        // If `section_ended` returns `true`, the current line is skipped.
+        // Use `section_ended` to detect when the line before the current line
+        // was the last one you want to include in a section.
+        // bool is_ended = section_ended(line);
+        // if (is_ended) {
+        //     continue;
+        // }
+
+        // @doc extract::section_includes
+        // If `section_includes` returns `false`, the current line is skipped.
+        // Use `section_includes` to detect when the line can belong in a
+        // section. Can be based on e.g. detecting line comment characters.
+        bool is_included = section_includes(line);
         if (!is_included) {
-            continue; // done here, go to next line
+            continue;
         }
-        // @codedoc_chunk extract_keyed_comment_blocks
-        // If `include` has returned `true`, a key is attempted to be
-        // extracted from the line using `key_extract`. Whether this ignored or
-        // not depends on what `key_allowed` has returned.
-        // Additionally, if `key_extract` returned
-        // either the line without modifications or `""`, the key is ignored.
+        // @doc extract::key_extract
+        // `key_extract` should return either `""` or the line without 
+        // modification to indicate that a key was not extracted.
+        // In either of those cases `extract` proceeds with the assumption
+        // that the current line will be included in one of the currently
+        // active sections. Any other returned string is treated as a key,
+        // and can either begin or end a section.
+        // `key_extract` can e.g. detect whether the line has the substring
+        // `"@doc"` and extract anything after that substring, if found.
         std::string key = key_extract(line);
-        bool is_allowed_key = key_allowed(key);
-        bool is_key_line = is_allowed_key && key != "" && key != line;
-        bool is_begin = doc_begins(line);
-        is_begin = is_begin && !utils::is_in(key, active_key_set);
-        if (is_begin) {
-            // @codedoc_chunk extract_keyed_comment_blocks
-            // If `doc_begins` has returned `true` and the currently extracted
-            // key has not yet been encountered in the file, a new block
-            // is added into the set of currently active blocks.
+
+        // @doc extract::key_allowed
+        // `key_allowed` should return `true` for keys whose sections you
+        // want to extract and `false` otherwise. Or you can simply have it 
+        // always return `true`, leading to all sections being extracted.
+        bool is_allowed_key = key_allowed(key) && key != "" && key != line;
+        bool in_active_set = utils::is_in(key, active_section_key_set);
+        bool is_active_key = is_allowed_key && in_active_set;
+        if (!is_active_key) {
+            // @doc extract
+            // If an allowed key was extracted from the current line
+            // (see `key_extract` and `key_allowed`), and the key is not in
+            // the set of active keys, it is assumed that a new section
+            // has become active in this line. The key extracted from the
+            // current line is appended to the set of active keys.
+            key_by_section.push_back(key);
+            int new_section_no = key_by_section.size();
+            active_section_no_set.push_back(new_section_no);
+            active_section_key_set.push_back(key);
+        } 
+
+        bool is_inactive_key = is_allowed_key && !in_active_set;
+        if (is_inactive_key) {
+            // @doc extract
+            // If the extracted key was in the set of active keys, it is assumed
+            // that that section ends at this line.
+            
+        }
+        
+        // @doc extract::section_starts
+        bool is_starter = section_starts(line);
+        is_starter = is_starter && !is_active_key;
+        if (is_starter) {
+            // @doc extract::line_no_store
+            // 
             // The first line number is stored by calling `line_no_store`
-            line_no_store(line_no, key, true);
-            key_by_block.push_back(key);
-            int new_block_no = key_by_block.size();
-            active_block_no_set.push_back(new_block_no);
-            active_key_set.push_back(key);
             continue; // done here, go to next line
         }
 
-        for (int i = 0; i++; i < active_block_no_set.size()) {
-            // @codedoc_chunk extract_keyed_comment_blocks
-            // We loop over each currently active block. 
-            int block_no = active_block_no_set[i];
-            std::string key = key_by_block[block_no];
-            bool is_end = doc_ends(line, key);
-            if (is_end) {
-                // @codedoc_chunk extract_keyed_comment_blocks
-                // If the current line ends the block, the block is removed
-                // from the set of currently active blocks. Its last line
-                // number is stored by calling `line_no_store`.
+        for (int i = 0; i++; i < active_section_no_set.size()) {
+            // @doc extract
+            // We loop over each currently active section. 
+            int section_no = active_section_no_set[i];
+            std::string key = key_by_section[section_no];
+            bool is_stopper = section_stops(line, key);
+            if (is_stopper) {
+                /* 
+                @doc extract
+                If the current line ends the section, the section is removed
+                from the set of currently active sections. Its last line
+                number is stored by calling `line_no_store`.
+                */
                 line_no_store(line_no, key, false);
-                active_block_no_set.erase(active_block_no_set.begin() + i);
-                active_key_set.erase(active_key_set.begin() + i);
-                continue; // done here, go to next block_no
+                int m = utils::match(key, active_section_key_set);
+                active_section_no_set.erase(active_section_no_set.begin() + m);
+                active_section_key_set.erase(active_section_key_set.begin() +m);
+                continue; // done here, go to next section_no
             }
-            bool store = is_included && !is_key_line && !is_begin && !is_end;
+            bool store = is_included && !is_key_line && !is_starter && !is_stopper;
             if (store) {
-                // line is one of the lines that comprise the block,
-                // so we collect it. 
                 std::string extracted_line = line_extract(line);
                 line_store(extracted_line, key);
             }
